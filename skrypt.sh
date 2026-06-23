bb56c989eb4bc8fba9f6b6fafe078f42f71a95e6
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-23 16:42:59 +0000

    Dodanie krótkich flagów

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-if [[ "$1" == "--date" ]]; then
+
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
 
-elif [[ "$1" == "--help" ]]; then
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "Dostępne opcje:"
-    echo "  --date           - Wyświetla datę"
-    echo "  --logs           - Tworzy 100 plików log[x].txt"
-    echo "  --logs [liczba]  - Tworzy podaną liczbę plików log[x].txt"
+    echo "  --date / -d        - Wyświetla datę"
+    echo "  --logs / -l        - Tworzy 100 plików log[x].txt"
+    echo "  --logs [n] / -l [n]- Tworzy n plików log[x].txt"
 
-elif [[ "$1" == "--logs" ]]; then
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     count=${2:-100}
     for i in $(seq 1 $count); do
         echo "log$i.txt, skrypt.sh, $(date)" > "log$i.txt"
     done
+
 else
-    echo "Nieznana opcja"
+    echo "Nieznana opcja. Użyj --help"
 fi
