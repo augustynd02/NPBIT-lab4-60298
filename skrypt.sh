92afbb98730d3c7988e17ddada8bc892520ac8a5
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-23 15:57:12 +0000

    Dodanie obsługi parametru --logs do tworzenia 100 logów

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [[ "$1" == "--date" ]]; then
     date
+elif [[ "$1" == "--logs" ]]; then
+    for i in {1..100}; do
+        echo "log$i.txt, skrypt.sh, $(date)" > "log$i.txt"
+    done
 else
     echo "Nieznana opcja"
 fi
