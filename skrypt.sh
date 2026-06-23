94b19df2d1bddb786d9d2ff5e3bdc4b8dfb3d3d1
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-23 16:07:14 +0000

    Dodanie obslugi parametru --help

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 if [[ "$1" == "--date" ]]; then
     date
+
+elif [[ "$1" == "--help" ]]; then
+    echo "Dostępne opcje:"
+    echo "  --date           - Wyświetla datę"
+    echo "  --logs           - Tworzy 100 plików log[x].txt"
+    echo "  --logs [liczba]  - Tworzy podaną liczbę plików log[x].txt"
+
 elif [[ "$1" == "--logs" ]]; then
     count=${2:-100}
     for i in $(seq 1 $count); do
