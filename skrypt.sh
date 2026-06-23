1afbcdb94b4ebb847204e15a06e5b62ac2069332
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-23 16:05:48 +0000

    Ulepszenie skryptu z parametrem --log do obslugi ilosci logów

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,8 @@
 if [[ "$1" == "--date" ]]; then
     date
 elif [[ "$1" == "--logs" ]]; then
-    for i in {1..100}; do
+    count=${2:-100}
+    for i in $(seq 1 $count); do
         echo "log$i.txt, skrypt.sh, $(date)" > "log$i.txt"
     done
 else
